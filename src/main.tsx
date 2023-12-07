ee2927350bd21c69fd6089abcaf0f3b379c9a137
Nebel MASS <52921005+Duorly@users.noreply.github.com>
2023-12-07 16:43:51 +0100

    Init My React Lib Project With A Empty ViteReactProject

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './assets/style.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+      <h1>Begin my lib</h1>
+  </React.StrictMode>,
+)
